6dfe137f3692bfd74350c14a75c338d1070f187f
ytake <yuuki.takezawa@comnect.jp.net>
2020-06-08 23:03:56 +0900

    Initial commit

---
+++ build-repo.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+set -e
+
+echo "** Building repository"
+set -x
+
+hhvm --hphp -t hhbc --module vendor --ffile run.hack \
+  --output-dir tmp \
+  --file-cache tmp/file.cache
+
+set +x
+
+echo "** Listing outputs"
+ls -lh tmp
